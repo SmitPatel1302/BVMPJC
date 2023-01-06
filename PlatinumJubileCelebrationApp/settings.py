c512859915f33fb260468d914f3717706e235597
smit <90560950+SmitPatel1302@users.noreply.github.com>
2023-01-06 20:42:23 +0530

    set debug=True

--- PlatinumJubileCelebrationApp/settings.py
+++ PlatinumJubileCelebrationApp/settings.py
@@ -14,7 +14,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-%7vxcda*#cnq&qha)ci(l6f_3-w&wy4qnm3-bf(6z7-)z&k+1!'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
